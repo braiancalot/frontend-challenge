3ffc0323666777a14e4113a162722abc2d98cd08
braiancalot <calot665@hotmail.com>
2025-05-15 08:14:33 -0300

    feat: improve CategoryFilter to use @headlessui RadioGroup and ts

---
+++ src/app/components/CategoryFilter.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useState } from "react";
+import { Radio, RadioGroup } from "@headlessui/react";
+
+const options = [
+  { label: "TODOS OS PRODUTOS", value: "all", className: "w-[169px]" },
+  { label: "CAMISETAS", value: "t-shirts", className: "w-[89px]" },
+  { label: "CANECAS", value: "mugs", className: "w-[72px]" },
+];
+
+export function CategoryFilter() {
+  const [filter, setFilter] = useState(options[0].value);
+
+  return (
+    <RadioGroup value={filter} onChange={setFilter} className="flex gap-10">
+      {options.map((option) => (
+        <Radio
+          key={option.value}
+          value={option.value}
+          className={`text-text-body cursor-pointer ${option.className} text-center border-orange-low data-checked:border-b-4 data-checked:-mb-1 data-checked:text-product-name data-checked:font-semibold`}
+        >
+          {option.label}
+        </Radio>
+      ))}
+    </RadioGroup>
+  );
+}
