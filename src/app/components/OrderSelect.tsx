98a4a420dc578cd58c4ca34d0aeac3a0e90015a3
braiancalot <calot665@hotmail.com>
2025-05-18 10:28:46 -0300

    fix: headless Listbox scrollbar hidden and modal in Opera

--- src/app/components/OrderSelect.tsx
+++ src/app/components/OrderSelect.tsx
@@ -24,25 +24,27 @@ interface OrderSelectProps {
 
 export function OrderSelect({ orderBy, setOrderBy }: OrderSelectProps) {
   return (
-    <Listbox value={orderBy} onChange={setOrderBy}>
-      <ListboxButton className="flex items-center gap-2 text-text-body text-sm outline-none cursor-pointer">
-        {orderBy?.label || "Ordenar por"}
-        <Image src={ChevromDown} alt="ChevromDown" />
-      </ListboxButton>
-      <ListboxOptions
-        anchor="bottom end"
-        className="bg-shape-1 mt-1 rounded-sm shadow px-3 py-2 outline-none flex flex-col"
-      >
-        {options.map((option) => (
-          <ListboxOption
-            key={option.value}
-            value={option}
-            className="cursor-pointer hover:bg-shape-5 px-1 py-1 text-text-body text-sm rounded-sm"
-          >
-            {option.label}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
-    </Listbox>
+    <div className="relative">
+      <Listbox value={orderBy} onChange={setOrderBy}>
+        <ListboxButton className="flex items-center gap-2 text-text-body text-sm outline-none cursor-pointer">
+          {orderBy?.label || "Ordenar por"}
+          <Image src={ChevromDown} alt="ChevromDown" />
+        </ListboxButton>
+        <ListboxOptions
+          modal={false}
+          className="bg-shape-1 mt-1 rounded-sm shadow px-3 py-2 outline-none flex flex-col absolute right-0 w-max"
+        >
+          {options.map((option) => (
+            <ListboxOption
+              key={option.value}
+              value={option}
+              className="cursor-pointer hover:bg-shape-5 px-1 py-1 text-text-body text-sm rounded-sm"
+            >
+              {option.label}
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </Listbox>
+    </div>
   );
 }
