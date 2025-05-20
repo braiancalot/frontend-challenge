7004648d62dbae8d41b98152fd435be964fc5dae
braiancalot <calot665@hotmail.com>
2025-05-19 21:18:27 -0300

    fix: convert product created_at in infra/db.ts

--- src/infra/db.ts
+++ src/infra/db.ts
@@ -1,10 +1,24 @@
 import fs from "fs";
 import path from "path";
 
+interface Product {
+  name: string;
+  description: string;
+  image_url: string;
+  category: string;
+  id: string;
+  price_in_cents: number;
+  sales: number;
+  created_at: string;
+}
+
 function getProducts() {
   const filePath = path.join("public", "products.json");
   const jsonData = fs.readFileSync(filePath, "utf-8");
-  const products = JSON.parse(jsonData).products;
+  const products = JSON.parse(jsonData).products.map((product: Product) => ({
+    ...product,
+    created_at: new Date(product.created_at),
+  }));
 
   return products;
 }
